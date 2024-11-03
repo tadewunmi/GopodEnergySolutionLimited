0492d3bf5564673d46ec998c66ccd509f74a74e7
tadewunmi <tadewunmi@autovin.com.ng>
2024-11-03 03:25:29 +0100

    Updates of padding for each section

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
 <ul>
-    <li><a href="/education-consult.php">Home</a></li>
+    <li><a href="#">Home</a></li>
     <li><a href="#about">About</a></li>
     <li><a href="#service">Services</a></li>
     <li><a href="#shop">Shop</a></li>
